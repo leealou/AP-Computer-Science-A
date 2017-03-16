d9ffba61e32a7afbfc388318155bd10cf437ca7d
Alexis Lee <AlexisLee1999@gmail.com>
2017-03-16 12:07:16 -0400

    Create Journal.java

---
+++ Classify Reading Materials/Journal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booktypes;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Journal extends ReadingMaterial 
+{  
+   public Journal(String type, int pages, int characters, int numPictures)  
+   {  
+     super("New England Journal of Medicine", 200, 1, 3);  
+   }    
+}  
