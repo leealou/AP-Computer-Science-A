1c2cd868a6ce30bd8ecff99b8b6ccd983731f703
Alexis Lee <AlexisLee1999@gmail.com>
2017-03-16 12:08:32 -0400

    Create Book.java

---
+++ Classify Reading Materials/Book.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booktypes;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Book extends ReadingMaterial 
+{  
+   public Book(String type, int pages, int characters, int numPictures)  
+   {  
+     super("Clifford: The Big Red Dog", 22, 5, 20);  
+   }    
+}  
