009ef48ea3301e62f5a6e371f8d43ea1f26095f3
Alexis Lee <AlexisLee1999@gmail.com>
2017-04-06 13:58:24 -0400

    Update CardTester.java

--- Elevens Lab/Activity 1/CardTester.java
+++ Elevens Lab/Activity 1/CardTester.java
@@ -1,13 +1,17 @@
-/**
+/* Author: Alexis Lee (leealou)
+ * 
  * This is a class that tests the Card class.
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ public static void main(String[] args) {
+  Card one = new Card("3", "Hearts", 3);
+  Card two = new Card("2", "Spades",3);
+  Card three = new Card("3", "Hearts", 3);
+  
+  System.out.println("One matches two?" + one.matches(two));
+  System.out.println("One matches three?" + one.matches(three));    
+  System.out.println("Suit of Card One: " + one.suit());
+  System.out.println("Card one: " + one.toString());
+ }
 }
